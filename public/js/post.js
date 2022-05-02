f43b3a9194fe57541040a115eb9b6514fbc4a7c1
phillyCheese <ppha1@student.cvtc.edu>
2022-05-01 22:20:56 -0500

    Moved JS code in index.ejs into it's own file
    Changelogs:
     - Removed logTask.js
     - Moved JS code from index.ejs into post.js

--- public/js/post.js
+++ public/js/post.js
@@ -1,11 +1,31 @@
-let createPost = document.querySelector('.createBtn');
+let title = document.querySelector('.title');
+let body = document.querySelector('.body');
+let submitPostBtn = document.querySelector('.submitPost');
 
-createPost.addEventListener(onclick, function(e) {
-	e.preventDefault;
+console.log(title.value + " hola  " + body.value);
 
+// logic to insert post
+const taskThatIShallComplete = async function () {
+	const response = await fetch('/log', {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			title: "'" + title.value + "'",
+			body: "'" + body.value + "'"
+		})
+	});
+	const result = await response.json();
+	console.log(result);
 
+};
 
+submitPostBtn.addEventListener('click', function (e) {
 
+	e.preventDefault();
 
-	
+	taskThatIShallComplete();
+
 });
