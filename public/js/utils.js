da5ffbf815d0701721fe53fe1f57624abb0d244c
mxiong112cvtc <mxiong112@student.cvtc.edu>
2022-05-10 17:38:26 -0500

    [Finishes #182123761] Fix error when searching for null or empty string

---
+++ public/js/utils.js
@@ -0,0 +1,29 @@
+// TOP OF THE PAGE BUTTON
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// SEARCHING
+function isSearchEmpty() {
+    var elementValue = document.querySelector("#searchBox").value;
+    if (elementValue == null || elementValue == "") {
+        return false;
+    };
+}
