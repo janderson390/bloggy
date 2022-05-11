7b3358319a26b3156a71e956062291e242a7a9ab
mxiong112cvtc <mxiong112@student.cvtc.edu>
2022-05-10 19:06:24 -0500

    [Finishes #182131811] Added label showing how many chars are left for a post

--- public/js/utils.js
+++ public/js/utils.js
@@ -6,23 +6,51 @@ window.onscroll = function() {
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+
 // SEARCHING
+let elementValue = document.querySelector("#searchBox").value;
+
 function isSearchEmpty() {
-    var elementValue = document.querySelector("#searchBox").value;
     if (elementValue == null || elementValue == "") {
         return false;
     };
 }
+
+
+// CHECKING CHARACTERS LEFT
+let postBody = document.querySelector("textarea.bodyTxtForm");
+let charsLeftSpan = document.querySelector("#charsLeftNum");
+let submitBtn = document.querySelector(".submitPost");
+
+const maxPostChars = 255;
+
+// Fires everytime the value changes
+postBody.addEventListener("input", function(e) {
+    // Grab length of postBody
+    let postLength = postBody.value.length;
+
+    // Update the charsLeftSpan
+    let charsLeft = maxPostChars - postLength;
+    charsLeftSpan.innerHTML = charsLeft;
+
+    // Change color of the charsLeftSpan if charsLeft is negative or not
+    charsLeftSpan.style.color = (charsLeft <= 0) ? "red" : "black";
+
+    // Check length of postBody text is more than 255
+    // If true, disable submit button, else enable it
+    submitBtn.disabled = (postBody.value.length > 255);
+
+});
