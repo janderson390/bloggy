455674d30dc0a3c082deb1621e82798ca486b0ba
phillycheesesteak123 <98784368+phillycheesesteak123@users.noreply.github.com>
2022-05-16 13:22:26 -0500

    Adjustments to CSS and repositioned create post button

--- public/js/showPostForm.js
+++ public/js/showPostForm.js
@@ -1,10 +1,19 @@
 let showBtn = document.querySelector('#showPostForm');
 let formDiv = document.querySelector('.postsForm');
+let exitPostBtn = document.querySelector('.exitPostBtn');
+const BUFFER = 25;
 
-showBtn.addEventListener('click', function(e) {
+
+showBtn.addEventListener('click', function (e) {
     e.preventDefault();
 
-    formDiv.style.display = 'block';
+    formDiv.style.display = 'initial';
     showBtn.style.display = 'none';
 
 });
+
+exitPostBtn.addEventListener('click', function () {
+    formDiv.style.display = 'none';
+    showBtn.style.display = 'initial';
+})
+
